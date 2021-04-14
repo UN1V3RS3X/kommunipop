250cd60a57424c99aa97b67d0ff0d8afcdb21aa5
Yamid Patiño <yamidandres25@gmail.com>
2021-04-13 23:08:10 -0500

    Page for some test

---
+++ lib/src/ui/initial_setting/initial_config.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class InitialConfig extends StatelessWidget {
+  const InitialConfig({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Name')),
+      body: Center(
+        child: Column(
+          children: <Widget>[],
+        ),
+      ),
+    );
+  }
+}
